cef43cf1e7e0251d239a14a3d9c760bbe6c40e2e
Arrhe <arrhenri@gmail.com>
2020-08-27 10:41:06 -0400

    still broken

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -13,8 +9,6 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import Container from '@material-ui/core/Container';
-import products from './products';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -28,6 +22,10 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
+ const Navbar = () =>{
+    const classes = useStyles();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -36,47 +34,6 @@ const useStyles = makeStyles((theme) => ({
   const handleClose = () => {
     setAnchorEl(null);
   };
-
-  const classes = useStyles();
-
-    const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const Navbar = () => {
-    
-
-    let addedItems = this.props.products.length ? (
-        this.props.products.map(products=>{
-            
-            return(
-               
-                <li className="collection-item avatar" key={products.id}>
-                        
-                            <div className="item-desc">
-                                <span className="title">{products.name}</span>
-                                <p><b>Price: ${products.price}</b></p> 
-                                <p>
-                                    <b>Quantity: {products.quantity}</b> 
-                                </p>
-                                <div className="add-remove">
-                                    <IconButton edge="start" color="inherit" aria-label="menu">
-                                        <ArrowDropUpIcon/>
-                                    </IconButton>
-                                    <IconButton edge="start" color="inherit" aria-label="menu">
-                                        <ArrowDropDownIcon/>
-                                    </IconButton>
-                                </div>
-                                    <IconButton edge="start" color="inherit" aria-label="menu">
-                                        <HighlightOffIcon/>
-                                    </IconButton>
-                            </div>
-                            
-                       </li>                        
-            )
-        }
-    )):
-    (
-        <p>Nothing.</p>
-    )
 
     return(
             // <nav className="nav-wrapper">
@@ -114,20 +71,13 @@ const useStyles = makeStyles((theme) => ({
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <Container>
-            <div class="cart">
-                <h5>You have ordered:</h5>
-                <ul>
-                    {addedItems}
-                </ul>
-            </div>
-        </Container>
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
       </Menu>
-
+      
     </div>
     )
-    
-
 }
 
 const mapStateToProps = (state) => {
@@ -136,4 +86,4 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps)(Navbar);
+export default Navbar;
