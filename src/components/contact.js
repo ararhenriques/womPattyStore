870d68c620c88e27910a0fd09b82d14d02267836
Matthew Flowers <mjflowers5000@gmail.com>
2020-10-01 19:40:23 -0400

    no message

--- src/components/contact.js
+++ src/components/contact.js
@@ -13,7 +13,7 @@ const styles = theme => ({
           backgroundSize: 'cover',
           backgroundImage: `url(${bgImage2})`,
           backgroundAttachment: "fixed",
-    
+
         }
       },
     aBox: {
@@ -25,6 +25,8 @@ const styles = theme => ({
         backgroundColor: '#122F54',
         boxShadow: 3,
         margin: '100px 0 50px auto',
+        padding: '15px',
+        position: 'relative'
     },
     mBox: {
         maxWidth: 700,
@@ -35,11 +37,45 @@ const styles = theme => ({
         backgroundColor: '#122F54',
         boxShadow: 3,
         margin: '100px auto 50px 0'
-    }
+    },
+    topleft: {
+        width: 0,
+        height: 0,
+        borderTop: '35px solid red',
+        borderRight: '35px solid transparent'
+     },
+
+     topright: {
+        postion: 'absolute',
+        marginLeft: '665px',
+        marginTop:'-35px',
+        marginBottom: '500px',
+        width: 0,
+        height: 0,
+        borderTop: '35px solid red',
+        borderLeft: '35px solid transparent'
+     },
+     bottomleft: {
+        postion: 'absolute',
+        marginBottom:'-35px',
+        width: 0,
+        height: 0,
+        borderBottom: '35px solid red',
+        borderRight: '35px solid transparent'
+     },
+     bottomright: {
+        postion: 'absolute',
+        marginLeft: '665px',
+        width: 0,
+        height: 0,
+        borderBottom: '35px solid red',
+        borderLeft: '35px solid transparent',
+      }
+
 });
 
 class Contact extends Component{
-    
+
     render() {
 
         const { classes } = this.props;
@@ -47,13 +83,17 @@ class Contact extends Component{
         return(
             <div>
                 <div className={classes.aBox}>
+                    <div className={classes.topleft}></div>
+                    <div className={classes.topright}></div>
                     <img></img><h1>Arris</h1>
+                    <div className={classes.bottomleft}></div>
+                    <div className={classes.bottomright}></div>
                 </div>
                 <div className={classes.mBox}>
                     <img></img><h1>Matt</h1>
                 </div>
             </div>
-            
+
         );
     };
 }
